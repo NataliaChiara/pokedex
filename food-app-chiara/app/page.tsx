39e630f4674f660d605e5c6a2f10b5c7b77111bf
NataliaChiara <81540550+NataliaChiara@users.noreply.github.com>
2024-03-17 20:23:54 -0300

    Add files via upload

---
+++ food-app-chiara/app/page.tsx
@@ -0,0 +1,43 @@
+import s from './page.module.css';
+
+export default function Home() {
+  return (
+    <main className={s.main}>
+      <p className={s.relleno}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum molestias eveniet,
+        reiciendis vero minima eius praesentium fugiat temporibus molestiae hic dignissimos, magni
+        dolor illum assumenda voluptate quis minus ex iure. Ducimus illo, voluptatibus ab dolores
+        est ipsum quod repudiandae facere dicta dolore, corrupti repellat officiis. Nam dolorem ex
+        repudiandae illo temporibus quisquam atque. Accusantium, rerum optio atque saepe cumque
+        omnis! Enim vero, maiores cumque laudantium esse aliquam? Harum maxime temporibus
+        cupiditate, eius aut, expedita sit corrupti quibusdam vero corporis, perspiciatis possimus
+        repudiandae architecto ab earum! Sapiente enim praesentium aspernatur hic. Ab, commodi qui
+        necessitatibus totam atque nihil optio beatae voluptatibus expedita dolore illo veritatis
+        corrupti quae, quod, veniam dolorem accusantium ex nobis natus doloribus libero temporibus
+        consequatur? Cumque, libero magni? Quae soluta iure laboriosam accusamus sequi, possimus
+        alias mollitia aperiam maiores. Quis enim eligendi asperiores autem numquam temporibus,
+        voluptatem at incidunt tenetur veritatis quaerat quasi cumque similique, possimus neque
+        suscipit?Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam aspernatur quae
+        ipsa asperiores nemo beatae nam rem deleniti dicta non, molestias in sed porro deserunt
+        mollitia, aperiam accusantium vitae dolor. Laboriosam eveniet laborum quibusdam tempora
+        pariatur temporibus autem cum delectus corrupti aut quasi nulla magni eligendi dolorum quo
+        sunt libero natus sed, aperiam tenetur nemo illum doloribus impedit eos. Sit? Aperiam
+        necessitatibus, natus saepe eveniet nihil id reprehenderit quo recusandae consequuntur iure
+        at fuga, libero corporis nam commodi aspernatur magni. Soluta recusandae autem ad animi
+        vitae itaque earum labore officia.
+      </p>
+      <div className={s.container}>
+        <div className={s.container__shape}>
+          <div className={s.container__shape__text}>
+            <h1>SALE SALE SALE SALE SALE SALE SALE SALE</h1>
+            <p>LE SALE SALE SALE SALE SALE SALE SALE SA</p>
+            <h1>SALE SALE SALE SALE SALE SALE SALE SALE</h1>
+            <p>LE SALE SALE SALE SALE SALE SALE SALE SA</p>
+            <h1>SALE SALE SALE SALE SALE SALE SALE SALE</h1>
+            <p>LE SALE SALE SALE SALE SALE SALE SALE SA</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
